cd7ed65cc8795e257125592fd4549bb07927291b
André Andrade <andrepalasandrade@gmail.com>
2019-10-17 17:54:23 +0100

    fix errors in swaggerhub api

--- swaggerHub.js
+++ swaggerHub.js
@@ -2,10 +2,12 @@ const axios = require('axios')
 
 module.exports = {
   updateSwaggerDefinition: async function(owner, api, definition) {
-    return axios.post(`https://api.swaggerhub.com/apis/${owner}/${api}`, {
-      definition: definition
-    }, {
-      Authorization: process.env.SWAGGERHUB_API_KEY
+    return axios.post(`https://api.swaggerhub.com/apis/${owner}/${api}`,
+      definition, {
+      headers: {
+        'Authorization': process.env.SWAGGERHUB_API_KEY,
+        'Content-Type': 'application/yaml'
+      }
     })
   }
 }
