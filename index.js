0f9c7ea4ba98a2aa1859e502d55a4581b57e625c
André Andrade <andrepalasandrade@gmail.com>
2019-10-17 17:54:53 +0100

    let function errors be thrown outside the function

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-import executor from 'main'
+const executor = require('./main')
 
 exports.handler = async (event) => {
 
   console.log("Received publish event")
 
-  let response
+  let executeRes = await executor.execute()
+  console.log("Published successfully")
 
-  try {
-    let executeRes = await executor.execute()
-    console.log("Published successfully")
-
-    response = {
-      statusCode: 200,
-      body: JSON.stringify('Updated successfully')
-    }
-  } catch(err) {
-    console.err("Failed to publish", err)
-
-    response = {
-      statusCode: 500,
-      body: JSON.stringify(err)
-    }
+  let response = {
+    statusCode: 200,
+    body: JSON.stringify('Updated successfully')
   }
 
   return response
